125ff7e707339cc7c1d9959d79b761dd7476e10f
Boris Grishechkin <bg1722@aol.com>
2014-02-03 19:42:24 +0100

    all contacts

--- make-site.bat
+++ make-site.bat
@@ -1,3 +1,6 @@
 copy parts\1head.part + parts\2keywords_contacts.part + parts\3lang.part + parts\3menu.part + parts\4contacts.part + parts\5copyright_end.part contacts.html
+copy contacts.html index.html
 copy parts\1head.part + parts\2keywords_contacts_en.part + parts\3lang.part + parts\3menu_en.part + parts\4contacts_en.part + parts\5copyright_end.part contacts_en.html
+copy contacts_en.html index_en.html
 copy parts\1head.part + parts\2keywords_contacts_ru.part + parts\3lang.part + parts\3menu_ru.part + parts\4contacts_ru.part + parts\5copyright_end.part contacts_ru.html
+copy contacts_ru.html index_ru.html
