d78e2bba61d7215006869c7f948d1db3c5f2cd80
Boris Grishechkin <bg1722@aol.com>
2014-02-04 18:05:21 +0100

    fix appearance in IE/opera (a garbage symbol in the end of .html file)

--- make-site.bat
+++ make-site.bat
@@ -1,6 +1,6 @@
-copy parts\1head.part + parts\2keywords_contacts.part    + parts\3lang.part + parts\3menu.part    + parts\4contacts.part    + parts\5copyright_end.part contacts.html
-copy parts\1head.part + parts\2keywords_contacts_en.part + parts\3lang.part + parts\3menu_en.part + parts\4contacts_en.part + parts\5copyright_end.part contacts_en.html
-copy parts\1head.part + parts\2keywords_contacts_ru.part + parts\3lang.part + parts\3menu_ru.part + parts\4contacts_ru.part + parts\5copyright_end.part contacts_ru.html
-copy parts\1head.part + parts\2keywords_index.part    + parts\3lang.part + parts\3menu.part    + parts\4index.part       + parts\5copyright_end.part index.html
-copy parts\1head.part + parts\2keywords_index_en.part + parts\3lang.part + parts\3menu_en.part + parts\4index_en.part    + parts\5copyright_end.part index_en.html
-copy parts\1head.part + parts\2keywords_index_ru.part + parts\3lang.part + parts\3menu_ru.part + parts\4index_ru.part    + parts\5copyright_end.part index_ru.html
+copy parts\1head.part + parts\2keywords_contacts.part    + parts\3lang.part + parts\3menu.part    + parts\4contacts.part    + parts\5copyright_end.part contacts.html/B
+copy parts\1head.part + parts\2keywords_contacts_en.part + parts\3lang.part + parts\3menu_en.part + parts\4contacts_en.part + parts\5copyright_end.part contacts_en.html/B
+copy parts\1head.part + parts\2keywords_contacts_ru.part + parts\3lang.part + parts\3menu_ru.part + parts\4contacts_ru.part + parts\5copyright_end.part contacts_ru.html/B
+copy parts\1head.part + parts\2keywords_index.part    + parts\3lang.part + parts\3menu.part    + parts\4index.part       + parts\5copyright_end.part index.html/B
+copy parts\1head.part + parts\2keywords_index_en.part + parts\3lang.part + parts\3menu_en.part + parts\4index_en.part    + parts\5copyright_end.part index_en.html/B
+copy parts\1head.part + parts\2keywords_index_ru.part + parts\3lang.part + parts\3menu_ru.part + parts\4index_ru.part    + parts\5copyright_end.part index_ru.html/B
