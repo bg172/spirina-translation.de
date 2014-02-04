331c36c42645b429c2ec9580eb64f7c64ba7473d
boris.grishechkin <boris.grishechkin@dlr.de>
2014-02-04 15:43:59 +0100

    + contacts on start page

--- make-site.bat
+++ make-site.bat
@@ -1,6 +1,6 @@
-copy parts\1head.part + parts\2keywords_contacts.part + parts\3lang.part + parts\3menu.part + parts\4contacts.part + parts\5copyright_end.part contacts.html
-copy contacts.html index.html
+copy parts\1head.part + parts\2keywords_contacts.part    + parts\3lang.part + parts\3menu.part    + parts\4contacts.part    + parts\5copyright_end.part contacts.html
 copy parts\1head.part + parts\2keywords_contacts_en.part + parts\3lang.part + parts\3menu_en.part + parts\4contacts_en.part + parts\5copyright_end.part contacts_en.html
-copy contacts_en.html index_en.html
 copy parts\1head.part + parts\2keywords_contacts_ru.part + parts\3lang.part + parts\3menu_ru.part + parts\4contacts_ru.part + parts\5copyright_end.part contacts_ru.html
-copy contacts_ru.html index_ru.html
+copy parts\1head.part + parts\2keywords_index.part    + parts\3lang.part + parts\3menu.part    + parts\4index.part       + parts\5copyright_end.part index.html
+copy parts\1head.part + parts\2keywords_index_en.part + parts\3lang.part + parts\3menu_en.part + parts\4index_en.part    + parts\5copyright_end.part index_en.html
+copy parts\1head.part + parts\2keywords_index_ru.part + parts\3lang.part + parts\3menu_ru.part + parts\4index_ru.part    + parts\5copyright_end.part index_ru.html
