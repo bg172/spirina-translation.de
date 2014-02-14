9a7073b411e44880a16e01136ec7a4db809a95bc
boris.grishechkin <boris.grishechkin@dlr.de>
2014-02-14 11:35:00 +0100

    middle column width is now flexible

--- make-site.bat
+++ make-site.bat
@@ -1,9 +1,12 @@
-copy parts\1head.part + parts\2keywords_index.part    + parts\3lang.part + parts\3menu.part    + parts\4index.part       + parts\5copyright.part + parts\6openstat.part + parts\7end.part index.html/B
-copy parts\1head.part + parts\2keywords_index_en.part + parts\3lang.part + parts\3menu_en.part + parts\4index_en.part    + parts\5copyright.part + parts\6openstat.part + parts\7end.part index_en.html/B
-copy parts\1head.part + parts\2keywords_index_ru.part + parts\3lang.part + parts\3menu_ru.part + parts\4index_ru.part    + parts\5copyright.part + parts\6openstat.part + parts\7end.part index_ru.html/B
-copy parts\1head.part + parts\2keywords_contacts.part    + parts\3lang.part + parts\3menu.part    + parts\4profile.part    + parts\5copyright.part + parts\6openstat.part + parts\7end.part profile.html/B
-copy parts\1head.part + parts\2keywords_contacts_en.part + parts\3lang.part + parts\3menu_en.part + parts\4profile_en.part + parts\5copyright.part + parts\6openstat.part + parts\7end.part profile_en.html/B
-copy parts\1head.part + parts\2keywords_contacts_ru.part + parts\3lang.part + parts\3menu_ru.part + parts\4profile_ru.part + parts\5copyright.part + parts\6openstat.part + parts\7end.part profile_ru.html/B
-copy parts\1head.part + parts\2keywords_contacts.part    + parts\3lang.part + parts\3menu.part    + parts\4contacts.part    + parts\5copyright.part + parts\6openstat.part + parts\7end.part contacts.html/B
-copy parts\1head.part + parts\2keywords_contacts_en.part + parts\3lang.part + parts\3menu_en.part + parts\4contacts_en.part + parts\5copyright.part + parts\6openstat.part + parts\7end.part contacts_en.html/B
-copy parts\1head.part + parts\2keywords_contacts_ru.part + parts\3lang.part + parts\3menu_ru.part + parts\4contacts_ru.part + parts\5copyright.part + parts\6openstat.part + parts\7end.part contacts_ru.html/B
+cd parts\
+copy 1head.part+2keywords_index.part   +3lang.part+4index1.part   +3menu.part   +4index2.part   +5copyright.part+6openstat.part+7end.part ..\index.html/B
+copy 1head.part+2keywords_index_en.part+3lang.part+4index1_en.part+3menu_en.part+4index2_en.part+5copyright.part+6openstat.part+7end.part ..\index_en.html/B
+copy 1head.part+2keywords_index_ru.part+3lang.part+4index1_ru.part+3menu_ru.part+4index2_ru.part+5copyright.part+6openstat.part+7end.part ..\index_ru.html/B
+
+copy 1head.part+2keywords_contacts.part   +3lang.part+4profile1.part  +3menu.part    +4profile2.part   +5copyright.part+6openstat.part+7end.part ..\profile.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang.part+4profile1_en.part+3menu_en.part+4profile2_en.part+5copyright.part+6openstat.part+7end.part ..\profile_en.html/B
+copy 1head.part+2keywords_contacts_ru.part+3lang.part+4profile1_ru.part+3menu_ru.part+4profile2_ru.part+5copyright.part+6openstat.part+7end.part ..\profile_ru.html/B
+
+copy 1head.part+2keywords_contacts.part   +3lang.part+3menu.part   +4contacts.part   +5copyright.part+6openstat.part+7end.part ..\contacts.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang.part+3menu_en.part+4contacts_en.part+5copyright.part+6openstat.part+7end.part ..\contacts_en.html/B
+copy 1head.part+2keywords_contacts_ru.part+3lang.part+3menu_ru.part+4contacts_ru.part+5copyright.part+6openstat.part+7end.part ..\contacts_ru.html/B
