5c8caea701c81b71a7b872843d9bb0f0a670e0a4
boris.grishechkin <boris.grishechkin@dlr.de>
2014-02-18 20:50:57 +0100

    some more order

--- make-site.bat
+++ make-site.bat
@@ -1,12 +1,13 @@
 cd parts\
-copy 1head.part+2keywords_index.part   +3lang.part+4index1.part   +3menu.part   +4index2.part   +5copyright.part+6openstat.part+7end.part ..\index.html/B
-copy 1head.part+2keywords_index_en.part+3lang.part+4index1_en.part+3menu_en.part+4index2_en.part+5copyright.part+6openstat.part+7end.part ..\index_en.html/B
-copy 1head.part+2keywords_index_ru.part+3lang.part+4index1_ru.part+3menu_ru.part+4index2_ru.part+5copyright.part+6openstat.part+7end.part ..\index_ru.html/B
+copy 1head.part+2keywords_index.part   +3lang.part+4col3.part   +3menu.part   +4index.part   +5copyright.part+6openstat.part+7end.part ..\index.html/B
+copy 1head.part+2keywords_index_en.part+3lang.part+4col3_en.part+3menu_en.part+4index_en.part+5copyright.part+6openstat.part+7end.part ..\index_en.html/B
+copy 1head.part+2keywords_index_ru.part+3lang.part+4col3_ru.part+3menu_ru.part+4index_ru.part+5copyright.part+6openstat.part+7end.part ..\index_ru.html/B
 
-copy 1head.part+2keywords_contacts.part   +3lang.part+4profile1.part  +3menu.part    +4profile2.part   +5copyright.part+6openstat.part+7end.part ..\profile.html/B
-copy 1head.part+2keywords_contacts_en.part+3lang.part+4profile1_en.part+3menu_en.part+4profile2_en.part+5copyright.part+6openstat.part+7end.part ..\profile_en.html/B
-copy 1head.part+2keywords_contacts_ru.part+3lang.part+4profile1_ru.part+3menu_ru.part+4profile2_ru.part+5copyright.part+6openstat.part+7end.part ..\profile_ru.html/B
+copy 1head.part+2keywords_contacts.part   +3lang.part+4col3.part  +3menu.part    +4profile.part   +5copyright.part+6openstat.part+7end.part ..\profile.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang.part+4col3_en.part+3menu_en.part+4profile_en.part+5copyright.part+6openstat.part+7end.part ..\profile_en.html/B
+copy 1head.part+2keywords_contacts_ru.part+3lang.part+4col3_ru.part+3menu_ru.part+4profile_ru.part+5copyright.part+6openstat.part+7end.part ..\profile_ru.html/B
 
 copy 1head.part+2keywords_contacts.part   +3lang.part+3menu.part   +4contacts.part   +5copyright.part+6openstat.part+7end.part ..\contacts.html/B
 copy 1head.part+2keywords_contacts_en.part+3lang.part+3menu_en.part+4contacts_en.part+5copyright.part+6openstat.part+7end.part ..\contacts_en.html/B
 copy 1head.part+2keywords_contacts_ru.part+3lang.part+3menu_ru.part+4contacts_ru.part+5copyright.part+6openstat.part+7end.part ..\contacts_ru.html/B
+cd..
