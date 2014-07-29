e975a16307173fcadc016c3b6164a956a5cc9b55
boris.grishechkin <boris.grishechkin@dlr.de>
2014-07-29 21:53:50 +0200

    teaching update to be continued

--- make-site.bat
+++ make-site.bat
@@ -23,4 +23,9 @@ copy 1head.part+2keywords_contacts.part   +3lang.part+4col3.part   +3menu.part  
 copy 1head.part+2keywords_contacts_en.part+3lang.part+4col3_en.part+3menu_en.part+4translation_en.part+5copyright.part+6openstat.part+7end.part ..\translation_en.html/B
 copy 1head.part+2keywords_contacts_ru.part+3lang.part+4col3_ru.part+3menu_ru.part+4translation_ru.part+5copyright.part+6openstat.part+7end.part ..\translation_ru.html/B
 
+
+copy 1head.part+2keywords_contacts.part   +3lang.part+4col3.part   +3menu.part   +4teaching.part   +5copyright.part+6openstat.part+7end.part ..\teaching.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang.part+4col3_en.part+3menu_en.part+4teaching_en.part+5copyright.part+6openstat.part+7end.part ..\teaching_en.html/B
+copy 1head.part+2keywords_contacts_ru.part+3lang.part+4col3_ru.part+3menu_ru.part+4teaching_ru.part+5copyright.part+6openstat.part+7end.part ..\teaching_ru.html/B
+
 cd..
