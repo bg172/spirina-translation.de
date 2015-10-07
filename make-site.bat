1e185139ef91e193c6d405deef1b5424fa62bc9e
bg1722 <bg1722@aol.com>
2015-10-07 19:40:49 +0200

    process critics 1

--- make-site.bat
+++ make-site.bat
@@ -1,39 +1,39 @@
 cd parts\
 copy 1head.part+2keywords_index.part   +3lang.part+4col3.part   +3menu1.part   +4index.part   +5copyright.part+6openstat.part+7end.part ..\index.html/B
-copy 1head.part+2keywords_index_en.part+3lang.part+4col3_en.part+3menu1_en.part+4index_en.part+5copyright.part+6openstat.part+7end.part ..\index_en.html/B
-copy 1head_ru.part+2keywords_index_ru.part+3lang.part+4col3_ru.part+3menu1_ru.part+4index_ru.part+5copyright.part+6openstat.part+7end.part ..\index_ru.html/B
+copy 1head.part+2keywords_index_en.part+3lang_en.part+4col3_en.part+3menu1_en.part+4index_en.part+5copyright.part+6openstat.part+7end.part ..\index_en.html/B
+copy 1head_ru.part+2keywords_index_ru.part+3lang_ru.part+4col3_ru.part+3menu1_ru.part+4index_ru.part+5copyright.part+6openstat.part+7end.part ..\index_ru.html/B
 
 copy 1head.part+2keywords_contacts.part   +3lang.part+4col3.part  +3menu2.part    +4profile.part   +5copyright.part+6openstat.part+7end.part ..\profile.html/B
-copy 1head.part+2keywords_contacts_en.part+3lang.part+4col3_en.part+3menu2_en.part+4profile_en.part+5copyright.part+6openstat.part+7end.part ..\profile_en.html/B
-copy 1head_ru.part+2keywords_contacts_ru.part+3lang.part+4col3_ru.part+3menu2_ru.part+4profile_ru.part+5copyright.part+6openstat.part+7end.part ..\profile_ru.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang_en.part+4col3_en.part+3menu2_en.part+4profile_en.part+5copyright.part+6openstat.part+7end.part ..\profile_en.html/B
+copy 1head_ru.part+2keywords_contacts_ru.part+3lang_ru.part+4col3_ru.part+3menu2_ru.part+4profile_ru.part+5copyright.part+6openstat.part+7end.part ..\profile_ru.html/B
 
 copy 1head.part+2keywords_contacts.part   +3lang.part+4col3.part  +3menu22.part    +4price.part   +5copyright.part+6openstat.part+7end.part ..\price.html/B
-copy 1head.part+2keywords_contacts_en.part+3lang.part+4col3_en.part+3menu22_en.part+4price_en.part+5copyright.part+6openstat.part+7end.part ..\price_en.html/B
-copy 1head_ru.part+2keywords_contacts_ru.part+3lang.part+4col3_ru.part+3menu22_ru.part+4price_ru.part+5copyright.part+6openstat.part+7end.part ..\price_ru.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang_en.part+4col3_en.part+3menu22_en.part+4price_en.part+5copyright.part+6openstat.part+7end.part ..\price_en.html/B
+copy 1head_ru.part+2keywords_contacts_ru.part+3lang_ru.part+4col3_ru.part+3menu22_ru.part+4price_ru.part+5copyright.part+6openstat.part+7end.part ..\price_ru.html/B
 
 copy 1head.part+2keywords_contacts.part   +3lang.part+4col3.part   +3menu3.part   +4interpretation.part   +5copyright.part+6openstat.part+7end.part ..\interpretation.html/B
-copy 1head.part+2keywords_contacts_en.part+3lang.part+4col3_en.part+3menu3_en.part+4interpretation_en.part+5copyright.part+6openstat.part+7end.part ..\interpretation_en.html/B
-copy 1head_ru.part+2keywords_contacts_ru.part+3lang.part+4col3_ru.part+3menu3_ru.part+4interpretation_ru.part+5copyright.part+6openstat.part+7end.part ..\interpretation_ru.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang_en.part+4col3_en.part+3menu3_en.part+4interpretation_en.part+5copyright.part+6openstat.part+7end.part ..\interpretation_en.html/B
+copy 1head_ru.part+2keywords_contacts_ru.part+3lang_ru.part+4col3_ru.part+3menu3_ru.part+4interpretation_ru.part+5copyright.part+6openstat.part+7end.part ..\interpretation_ru.html/B
 
 copy 1head.part+2keywords_contacts.part   +3lang.part+4col3.part   +3menu4.part   +4translation.part   +5copyright.part+6openstat.part+7end.part ..\translation.html/B
-copy 1head.part+2keywords_contacts_en.part+3lang.part+4col3_en.part+3menu4_en.part+4translation_en.part+5copyright.part+6openstat.part+7end.part ..\translation_en.html/B
-copy 1head_ru.part+2keywords_contacts_ru.part+3lang.part+4col3_ru.part+3menu4_ru.part+4translation_ru.part+5copyright.part+6openstat.part+7end.part ..\translation_ru.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang_en.part+4col3_en.part+3menu4_en.part+4translation_en.part+5copyright.part+6openstat.part+7end.part ..\translation_en.html/B
+copy 1head_ru.part+2keywords_contacts_ru.part+3lang_ru.part+4col3_ru.part+3menu4_ru.part+4translation_ru.part+5copyright.part+6openstat.part+7end.part ..\translation_ru.html/B
 
 copy 1head.part+2keywords_contacts.part   +3lang.part+4col3.part   +3menu5.part   +4customers.part   +5copyright.part+6openstat.part+7end.part ..\customers.html/B
-copy 1head.part+2keywords_contacts_en.part+3lang.part+4col3_en.part+3menu5_en.part+4customers_en.part+5copyright.part+6openstat.part+7end.part ..\customers_en.html/B
-copy 1head_ru.part+2keywords_teaching_ru.part+3lang.part+4col3_ru.part+3menu5_ru.part+4customers_ru.part+5copyright.part+6openstat.part+7end.part ..\customers_ru.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang_en.part+4col3_en.part+3menu5_en.part+4customers_en.part+5copyright.part+6openstat.part+7end.part ..\customers_en.html/B
+copy 1head_ru.part+2keywords_teaching_ru.part+3lang_ru.part+4col3_ru.part+3menu5_ru.part+4customers_ru.part+5copyright.part+6openstat.part+7end.part ..\customers_ru.html/B
 
 copy 1head.part+2keywords_contacts.part   +3lang.part+4col3.part   +3menu6.part   +4teaching.part   +5copyright.part+6openstat.part+7end.part ..\teaching.html/B
-copy 1head.part+2keywords_contacts_en.part+3lang.part+4col3_en.part+3menu6_en.part+4teaching_en.part+5copyright.part+6openstat.part+7end.part ..\teaching_en.html/B
-copy 1head_ru.part+2keywords_contacts_ru.part+3lang.part+4col3_ru.part+3menu6_ru.part+4teaching_ru.part+5copyright.part+6openstat.part+7end.part ..\teaching_ru.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang_en.part+4col3_en.part+3menu6_en.part+4teaching_en.part+5copyright.part+6openstat.part+7end.part ..\teaching_en.html/B
+copy 1head_ru.part+2keywords_contacts_ru.part+3lang_ru.part+4col3_ru.part+3menu6_ru.part+4teaching_ru.part+5copyright.part+6openstat.part+7end.part ..\teaching_ru.html/B
 
 copy 1head.part+2keywords_contacts.part   +3lang.part+4col3.part   +3menu8.part   +4message.part   +5copyright.part+6openstat.part+7end.part ..\message.html/B
-copy 1head.part+2keywords_contacts_en.part+3lang.part+4col3_en.part+3menu8_en.part+4message_en.part+5copyright.part+6openstat.part+7end.part ..\message_en.html/B
-copy 1head_ru.part+2keywords_contacts_ru.part+3lang.part+4col3_ru.part+3menu8_ru.part+4message_ru.part+5copyright.part+6openstat.part+7end.part ..\message_ru.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang_en.part+4col3_en.part+3menu8_en.part+4message_en.part+5copyright.part+6openstat.part+7end.part ..\message_en.html/B
+copy 1head_ru.part+2keywords_contacts_ru.part+3lang_ru.part+4col3_ru.part+3menu8_ru.part+4message_ru.part+5copyright.part+6openstat.part+7end.part ..\message_ru.html/B
 
 copy 1head.part+2keywords_contacts.part   +3lang.part+4col3.part   +3menu9.part   +4impressum.part   +5copyright.part+6openstat.part+7end.part ..\impressum.html/B
-copy 1head.part+2keywords_contacts_en.part+3lang.part+4col3_en.part+3menu9_en.part+4impressum_en.part+5copyright.part+6openstat.part+7end.part ..\impressum_en.html/B
-copy 1head_ru.part+2keywords_contacts_ru.part+3lang.part+4col3_ru.part+3menu9_ru.part+4impressum_ru.part+5copyright.part+6openstat.part+7end.part ..\impressum_ru.html/B
+copy 1head.part+2keywords_contacts_en.part+3lang_en.part+4col3_en.part+3menu9_en.part+4impressum_en.part+5copyright.part+6openstat.part+7end.part ..\impressum_en.html/B
+copy 1head_ru.part+2keywords_contacts_ru.part+3lang_ru.part+4col3_ru.part+3menu9_ru.part+4impressum_ru.part+5copyright.part+6openstat.part+7end.part ..\impressum_ru.html/B
 
 
 cd..
