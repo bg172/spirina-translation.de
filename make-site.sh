b22a0d2f057dbb9095962126b06109f9d8a352b6
bg <bg1722@aol.com>
2018-01-29 21:32:45 +0100

    title corrected

---
+++ make-site.sh
@@ -0,0 +1,35 @@
+cd parts
+cat 1head.part 2keywords_index.part    3lang.part 4col3.part    3menu1.part    4index.part    5copyright.part 6openstat.part 7end.part >../index.html
+cat 1head.part 2keywords_index_en.part 3lang_en.part 4col3_en.part 3menu1_en.part 4index_en.part 5copyright.part 6openstat.part 7end.part >../index_en.html
+cat 1head_ru.part 2keywords_index_ru.part 3lang_ru.part 4col3_ru.part 3menu1_ru.part 4index_ru.part 5copyright.part 6openstat.part 7end.part >../index_ru.html
+
+cat 1head.part 2keywords_contacts.part    3lang.part 4col3.part   3menu2.part     4medtourism.part    5copyright.part 6openstat.part 7end.part >../medtourism.html
+cat 1head.part 2keywords_contacts_en.part 3lang_en.part 4col3_en.part 3menu2_en.part 4medtourism_en.part 5copyright.part 6openstat.part 7end.part >../medtourism_en.html
+cat 1head_ru.part 2keywords_contacts_ru.part 3lang_ru.part 4col3_ru.part 3menu2_ru.part 4medtourism_ru.part 5copyright.part 6openstat.part 7end.part >../medtourism_ru.html
+
+cat 1head.part 2keywords_contacts.part    3lang.part 4col3.part   3menu3.part     4profile.part    5copyright.part 6openstat.part 7end.part >../profile.html
+cat 1head.part 2keywords_contacts_en.part 3lang_en.part 4col3_en.part 3menu3_en.part 4profile_en.part 5copyright.part 6openstat.part 7end.part >../profile_en.html
+cat 1head_ru.part 2keywords_contacts_ru.part 3lang_ru.part 4col3_ru.part 3menu3_ru.part 4profile_ru.part 5copyright.part 6openstat.part 7end.part >../profile_ru.html
+
+cat 1head.part 2keywords_contacts.part    3lang.part 4col3.part   3menu22.part     4price.part    5copyright.part 6openstat.part 7end.part >../price.html
+cat 1head.part 2keywords_contacts_en.part 3lang_en.part 4col3_en.part 3menu22_en.part 4price_en.part 5copyright.part 6openstat.part 7end.part >../price_en.html
+cat 1head_ru.part 2keywords_contacts_ru.part 3lang_ru.part 4col3_ru.part 3menu22_ru.part 4price_ru.part 5copyright.part 6openstat.part 7end.part >../price_ru.html
+
+cat 1head.part 2keywords_contacts.part    3lang.part 4col3.part    3menu4.part    4translation.part    5copyright.part 6openstat.part 7end.part >../translation.html
+cat 1head.part 2keywords_contacts_en.part 3lang_en.part 4col3_en.part 3menu4_en.part 4translation_en.part 5copyright.part 6openstat.part 7end.part >../translation_en.html
+cat 1head_ru.part 2keywords_contacts_ru.part 3lang_ru.part 4col3_ru.part 3menu4_ru.part 4translation_ru.part 5copyright.part 6openstat.part 7end.part >../translation_ru.html
+
+cat 1head.part 2keywords_contacts.part    3lang.part 4col3.part    3menu5.part    4customers.part    5copyright.part 6openstat.part 7end.part >../customers.html
+cat 1head.part 2keywords_contacts_en.part 3lang_en.part 4col3_en.part 3menu5_en.part 4customers_en.part 5copyright.part 6openstat.part 7end.part >../customers_en.html
+cat 1head_ru.part 2keywords_teaching_ru.part 3lang_ru.part 4col3_ru.part 3menu5_ru.part 4customers_ru.part 5copyright.part 6openstat.part 7end.part >../customers_ru.html
+
+cat 1head.part 2keywords_contacts.part    3lang.part 4col3.part    3menu6.part    4teaching.part    5copyright.part 6openstat.part 7end.part >../teaching.html
+cat 1head.part 2keywords_contacts_en.part 3lang_en.part 4col3_en.part 3menu6_en.part 4teaching_en.part 5copyright.part 6openstat.part 7end.part >../teaching_en.html
+cat 1head_ru.part 2keywords_contacts_ru.part 3lang_ru.part 4col3_ru.part 3menu6_ru.part 4teaching_ru.part 5copyright.part 6openstat.part 7end.part >../teaching_ru.html
+
+cat 1head.part 2keywords_contacts.part    3lang.part 4col3.part    3menu9.part    4impressum.part    5copyright.part 6openstat.part 7end.part >../impressum.html
+cat 1head.part 2keywords_contacts_en.part 3lang_en.part 4col3_en.part 3menu9_en.part 4impressum_en.part 5copyright.part 6openstat.part 7end.part >../impressum_en.html
+cat 1head_ru.part 2keywords_contacts_ru.part 3lang_ru.part 4col3_ru.part 3menu9_ru.part 4impressum_ru.part 5copyright.part 6openstat.part 7end.part >../impressum_ru.html
+
+
+cd ..
